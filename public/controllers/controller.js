6d68a02053e3c1c5c7dd9d0459cce5d0451190ce
Sebastian Selin <Sebastian Selin>
2017-03-22 19:47:32 +0100

    Finally works

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,22 +1,76 @@
- var myApp = angular.module('myApp', []);
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-console.log("Controller works");
-    
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
+  console.log("Controller works");
 
-$http.get('/').then(function(data){
-    console.log("I got the data I requested!");
-    $scope.customers = data;
-}); 
-    
-$http.get('/customers').then(function(data){
-    console.log("Customers !!!!!");
-    $scope.customers = data;
-}); 
 
-    
-$scope.addContact = function(){
-    console.log($scope.addContact);
-};
-                 
+
+  $http.get('/').then(function (data) {
+    //console.log("I got the data I requested!");
+    //$scope.todotable = data.todotable;
+  });
+
+
+  $scope.addItem = function (form) {
+    console.log(form.todo.title);
+    $http.post('/addData', form.todo).then(function successCallback(response) {
+      console.log("add data to database");
+    }, function errorCallback(response) {
+      console.log("failed to add data");
+      
+    });
+
+  };
+    $scope.deleteItem = function (id) {
+        console.log(id);
+        $http.get('/delData?id='+ id).then(function successCallback(response) {
+            console.log("deleted data from database");
+        }, function errorCallback(response) {
+            console.log("failed to add data");
+
+        });
+
+    };
+    $scope.editItem = function (form) {
+        console.log("hej");
+        $http.get('/editData?id='+ form.listItem.id + "&title=" + form.listItem.title + "&text=" + form.listItem.text).then(function successCallback(response) {
+            console.log("Data has been updated");
+        }, function errorCallback(response) {
+            console.log("failed to add data");
+
+        });
+
+    };
+
+  $http.get('http://localhost:3000/todotable').then(function (data) {
+    $scope.list = data.data
+    console.log($scope.list);
+  });
+
+  /* $(document).ready(function(){
+     $("deleteItem").on("click", function(){
+       $(this).closest("td").remove(); 
+     });
+   }); */
+
+
+
 }]);
 
+
+
+function swapStyleSheet(sheet) {
+    document.getElementById("pagestyle").setAttribute("href", sheet);  
+}
+
+function initate() {
+    var style1 = document.getElementById("stylesheet1");
+    var style2 = document.getElementById("stylesheet2");
+    var style3 = document.getElementById("stylesheet3");
+    var style4 = document.getElementById("stylesheet4");
+
+    style1.onclick = function () { swapStyleSheet("style.css") };
+    style2.onclick = function () { swapStyleSheet("red.css"); };
+    style3.onclick = function () { swapStyleSheet("black.css") };
+    style4.onclick = function () { swapStyleSheet("pruple.css"); };
+}
+window.onload = initate;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
 
+
+// Variable Connection to MySQL and My Raspberry Pi
 var connection = mysql.createConnection({
-    //properties
     host: '192.168.48.241',
     user: 'schwebbe',
     password: 'roflcopter',
@@ -12,36 +14,80 @@ var connection = mysql.createConnection({
 });
 
 
-
-
 app.use(express.static(__dirname + "/public"));
-
-app.get('/', function(req, res){
+app.use(bodyParser.json());
+app.get('/', function (req, res) {
     res.send('index.html');
 });
 
 
-app.get('/customers', function (req, res) {
+app.get('/todotable', function (req, res) {
     console.log('I recieved a GET request')
-    connection.query("SELECT * FROM customers", function (error, rows, fields) {
-      
+
+    connection.query("SELECT * FROM todotable", function (error, rows, fields) {
+
         //callback
         if (!error) {
-            console.log('Successful query', rows);
+            console.log('Successful query');
             res.send(JSON.stringify(rows));
+
         } else {
             console.log('Error while performing Query');
-            //parse with your rows/fields 
-            
+            //parse with your rows/fields  
         }
-        connection.end();
-
 
     });
-
+    //connection.end();
 });
+app.post('/addData', function (req, res) {
+    console.log(req.body.title);
+    connection.query("INSERT INTO todotable (title, text) VALUES('" + req.body.title + "','" + req.body.text + "')", function (error, rows, fields) {
+        if (!error) {
+            console.log(req.body.text + " updated")
+        } else
+            console.log("Fack" + error);
+    });
+    console.log(res[0]);
+    res.end();
+})
 
-app.post('/customers')
+app.get('/delData', function (req, res) {
+    console.log(req.query.id);
+    connection.query("DELETE FROM todotable WHERE id=" + req.query.id, function (error, rows, fields) {
+        if (!error) {
+            console.log(req.query.id + " updated")
+        } else
+            console.log("kalops" + error);
+    });
+    console.log(res[0]);
+    res.end();
+})
+
+app.get('/editData', function (req, res) {
+    console.log(req.body.title);
+    connection.query("UPDATE todotable SET Title='" + req.query.title + "', Text='"+ req.query.text +"'  where id=" + req.query.id, function(error, rows, fields) {
+        if (!error) {
+            console.log(req.body.text + " updated")
+        } else
+            console.log("Fack" + error);
+    });
+})
+/*app.get('/editData', function (req, res) {
+    console.log(req.query.id);
+    console.log(req.query.title);
+    console.log(req.query.text);
+    connection.query("UPDATE todotable SET title='"+req.query.title+"' WHERE id=, function (error, rows, fields) {
+        if (!error) {
+            console.log(req.query.id + " updated")
+        } else
+            console.log("kalops" + error);
+    });
+    console.log(res[0]);
+    res.end();
+}) */
+
+
+
 
 app.listen(3000);
-console.log("Server running on port 3000");
+console.log("server is running on port 3000");
